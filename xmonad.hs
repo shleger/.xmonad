b8516a11d29d95c9672ada0f94effa76f3b06b0f
Andrey_Shleger <Andrey_Shleger@epam.com>
2021-01-01 23:50:17 +0300

    add xmobar

--- xmonad.hs
+++ xmonad.hs
@@ -1,12 +1,32 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Gaps
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+-- NOTES: 0.10 works much better than 0.9, unfortunately distros mostly package 0.9 atm
+-- xmobar and fullscreen flash vids (youtube): http://code.google.com/p/xmobar/issues/detail?id=41
+
+-- TODO: would still like fullscreen flash vids to not crop and leave xmobar drawn
+-- TODO: remove the red border when doing fullscreen? tried adding 'smartBorders' to the layoutHook but that didn't work
+-- TODO: hook in TopicSpaces, start specific apps on specific workspaces
 
 main = do
-  xmonad $ defaultConfig
-    { terminal    = myTerminal
-    , modMask     = myModMask
-    , borderWidth = myBorderWidth
-    }
+  xmproc <- spawnPipe "~/.local/bin/xmobar ~/.xmonad/.xmobarrc"
+  xmonad defaultConfig {
+    modMask = mod4Mask, 
+    terminal = "kitty",
+    manageHook = manageDocks <+> manageHook defaultConfig,
+    layoutHook = avoidStruts $ layoutHook defaultConfig,
+    handleEventHook = mconcat [ docksEventHook, handleEventHook defaultConfig ],
 
-myTerminal    = "urxvt"
-myModMask     = mod4Mask -- Win key or Super_L
-myBorderWidth = 3
+    logHook = dynamicLogWithPP $ xmobarPP
+                        { ppOutput = hPutStrLn xmproc,
+                          ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+  }
+
