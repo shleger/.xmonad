2b8eb08260480d9f44feea961faac8e378697626
Andrey_Shleger <Andrey_Shleger@epam.com>
2021-01-16 16:49:34 +0300

    add additional keys

--- xmonad.hs
+++ xmonad.hs
@@ -6,7 +6,9 @@ import XMonad.Layout.NoBorders
 import XMonad.Layout.Gaps
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Actions.CycleWS
 import System.IO
+
 
 -- NOTES: 0.10 works much better than 0.9, unfortunately distros mostly package 0.9 atm
 -- xmobar and fullscreen flash vids (youtube): http://code.google.com/p/xmobar/issues/detail?id=41
@@ -14,10 +16,15 @@ import System.IO
 -- TODO: would still like fullscreen flash vids to not crop and leave xmobar drawn
 -- TODO: remove the red border when doing fullscreen? tried adding 'smartBorders' to the layoutHook but that didn't work
 -- TODO: hook in TopicSpaces, start specific apps on specific workspaces
+--
+-- Dont foget to link for xmobar --recompile && xmobar --restart:
+-- sudo ln -s ~/.local/bin/xmonad /usr/bin 
+--
 
 main = do
   xmproc <- spawnPipe "~/.local/bin/xmobar ~/.xmonad/.xmobarrc"
-  xmonad defaultConfig {
+  --xmproc <- spawnPipe "stalonetray"
+  xmonad $ defaultConfig {
     modMask = mod4Mask, 
     terminal = "kitty",
     manageHook = manageDocks <+> manageHook defaultConfig,
@@ -28,5 +35,13 @@ main = do
                         { ppOutput = hPutStrLn xmproc,
                           ppTitle = xmobarColor "green" "" . shorten 50
                         }
-  }
+  } `additionalKeys`
+    [ ((mod4Mask .|. shiftMask, xK_p), spawn "~/my/lockOut.sh"),
+      ((mod4Mask .|. shiftMask, xK_s), spawn "flameshot gui"), 
+      ((mod4Mask, xK_d), spawn "rofi -show run -lines 5 -opacity \"65\""),
+      ((mod4Mask, xK_comma), toggleWS),
+--      ((mod4Mask .|. shiftMask, xK_r ), spawn "xmonad --recompile && xmonad --restart"), -- similar to mod+q
+      ((mod4Mask .|. shiftMask, xK_x ), spawn "xterm"),
+      ((mod4Mask, xK_c), spawn "google-chrome-stable --force-device-scale-factor=1.3 --password-store=gnome") 
+    ]
 
